5d75753d7e298a1ff7e01c4facecea60aec1f266
Nik Hammer-Ellis <nikhammerellis@gmail.com>
2015-04-10 14:17:37 -0700

    rails black belt exam code

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def create
+  	user = User.authenticate(params[:session][:email], params[:session][:password])
+  	if user.nil?
+  		flash[:error] = 'Invalid Login Credentials'
+  		#if there is an error, redirect back to login
+  		redirect_to '/'
+  	else 
+  		sign_in user
+  		redirect_to '/ideas'
+  	end
+  end
+
+  def destroy
+  	sign_out
+  	redirect_to '/'
+  end
+end
